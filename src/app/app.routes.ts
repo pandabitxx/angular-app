752fc61c1b0438f07a2709533eee1ef0b3f4ca82
Anderson Gonzalez Acosta <pandabitxx@gmail.com>
2023-05-09 14:44:31 -0500

    styles login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import {RouterModule, Routes} from "@angular/router";
 import { UserFormComponent } from "./pages/users/user-form/user-form.component";
 import { ModuleWithProviders } from "@angular/core";
 import { UserListComponent } from "./pages/users/user-list/user-list.component";
+import { LoginComponent } from "./pages/layout/login/login.component";
 
 export const appRoutes: Routes = [
+    {path: '', component:  LoginComponent},
     {path: 'list', component:  UserListComponent},
     {path: 'form', component:  UserFormComponent},
 
