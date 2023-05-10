107bf69572fa5ee4219b5900b78b81682ba375d8
Anderson Gonzalez Acosta <pandabitxx@gmail.com>
2023-05-09 20:18:55 -0500

    form add user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { UserFormComponent } from "./pages/users/user-form/user-form.component";
 import { ModuleWithProviders } from "@angular/core";
 import { UserListComponent } from "./pages/users/user-list/user-list.component";
 import { LoginComponent } from "./pages/layout/login/login.component";
+import { HomeComponent } from "./pages/layout/home/home.component";
 
 export const appRoutes: Routes = [
     {path: '', component:  LoginComponent},
     {path: 'list', component:  UserListComponent},
     {path: 'form', component:  UserFormComponent},
+    {path: 'home', component:  HomeComponent},
 
+
+    //Lazy loading
     {
     path: 'list',
     loadChildren: () =>
--- src/app/pages/users/user-add/user-add.component.ts
+++ src/app/pages/users/user-add/user-add.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from 'src/app/common/navbar/navbar.component';
 import { ToolbarComponent } from 'src/app/common/toolbar/toolbar.component';
+import { ContactoUsuario } from '../user-form/models/contacto.usuario';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-add',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, ToolbarComponent],
+  imports: [CommonModule, FormsModule, NavbarComponent, ToolbarComponent],
   templateUrl: './user-add.component.html',
   styleUrls: ['./user-add.component.css']
 })
-export class UserAddComponent {
 
+export class UserAddComponent implements OnInit {
+  public usuario: ContactoUsuario;
+
+  constructor(){
+    this.usuario = new ContactoUsuario('','','')
+  }
+
+  ngOnInit() {
+
+  }
+
+  onSubmit(){
+    console.log('Evento submit lanzado!!');
+    console.log(this.usuario);
+  }
 }
